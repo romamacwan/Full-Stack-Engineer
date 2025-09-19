13c0d94f23fb078f5345a339f2d09e8069ac3293
Roma Macwan <romamacwan2427@gmail.com>
2025-09-19 15:00:51 -0400

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -9,6 +9,19 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function() { console.log('Server started on port 3000'); });
 
+// append this code when creating db and call shutting down db
+process.on('SIGINT', () => {
+    // when we kill server with Ctrl + C, we will go through every single web socket connection and close them
+    // then close the server
+    // then shut down the db
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    });
+});
+
 
 // BEGIN WEBSOCKET //
 
@@ -26,6 +39,11 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server');    // Sending message to the client who gets connected
     }
 
+    // append this code when creating db
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);   // broadcasting message number of clients to all connected clients
         console.log('A client has disconnected');   // sending a message to client on disconnection of a client
@@ -36,4 +54,32 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
+}
+
+// END WEBSOCKET //
+
+
+// BEGIN DATABASE //
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {        // ensures the db is setup before we run any queries
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
 }
